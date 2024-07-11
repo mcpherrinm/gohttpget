7e2f0f6558a20bdff20f9abd593900410bc365d4
Matthew McPherrin <git@mcpherrin.ca>
2024-07-11 15:38:38 -0400

    Add some error handling

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "expected 1 argument: url to GET")
+		os.Exit(1)
+	}
 	resp, err := http.Get(os.Args[1])
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "error fetching: %v", err)
+		os.Exit(1)
 	}
 	io.Copy(os.Stdout, resp.Body)
 }
